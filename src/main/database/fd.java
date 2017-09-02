76a667382396cc5a25b86371075a065e86eb53a3
loukle <1095190848@qq.com>
2017-09-02 14:35:01 +0800

    用户登陆注册servlet

--- src/main/database/fd.java
+++ src/main/database/fd.java
@@ -1,10 +1,54 @@
 package main.database;
 
+import main.Beans.SystemUser;
+import main.database.dbInterface.UserDao;
+
+import java.util.List;
+
 /**
  * Created by lee on 2017/9/1.
  *
  * @author: lee
  * create time: 下午1:12
  */
-public class fd {
+public class fd implements UserDao {
+    @Override
+    public SystemUser findUserByID(String ID) {
+        return null;
+    }
+
+    @Override
+    public SystemUser findUserByName(String userName) {
+        return null;
+    }
+
+    @Override
+    public void UpdateUser(SystemUser user) {
+
+    }
+
+    @Override
+    public String CreateUser(SystemUser user) {
+        return null;
+    }
+
+    @Override
+    public String DeleteUser(SystemUser user) {
+        return null;
+    }
+
+    @Override
+    public String DeleteUser(String userID) {
+        return null;
+    }
+
+    @Override
+    public List<SystemUser> searchUserByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<SystemUser> searchUserByEmail(String email) {
+        return null;
+    }
 }
---
+++ src/main/servlet/UserLoginServlet.java
@@ -0,0 +1,45 @@
+package main.servlet;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import main.Beans.SystemUser;
+import main.database.dbInterface.UserDao;
+import main.database.fd;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class UserLoginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        String user_name = request.getParameter("username");
+        String user_password=request.getParameter("userpassword");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        String signal="success";
+
+
+        try {
+
+            UserDao dao = new fd();
+            SystemUser user = dao.findUserByName("user_name");
+            user_password.equals(user.getPassword());
+            HttpSession session = request.getSession();
+            session.setAttribute("user",user);
+        } catch (Exception e){
+            signal="fail";
+            e.printStackTrace();
+        }
+        JsonObject object = new JsonObject();
+
+        object.addProperty("signal",signal);
+
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
