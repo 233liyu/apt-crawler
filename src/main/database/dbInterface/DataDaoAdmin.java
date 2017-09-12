284243d34d4ff224d4ab2142e2893da7b6c2e975
congge27 <784466409@qq.com>
2017-09-12 23:02:12 +0800

    userdaoadmin updated.

--- src/main/database/dbInterface/DataDaoAdmin.java
+++ src/main/database/dbInterface/DataDaoAdmin.java
@@ -11,11 +11,11 @@ import main.Beans.Data;
 public interface DataDaoAdmin {
     // catch exception and roll back, then throw a new Exception
 
-    public void UpdateDataByID(Data data);
+    public void UpdateDataByID(Data data) throws Exception;
 
     // trigger here!
-    public void DeleteData(String DataID);
+    public void DeleteData(String DataID) throws Exception;
 
-    public void CreateData(Data data);
+    public void CreateData(Data data) throws Exception;
 
 }
